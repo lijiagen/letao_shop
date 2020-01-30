fab386cc9a687b5adf8f831b4bed3d1f43e4cbd8
leesi <254589252@qq.com>
2020-01-30 15:35:16 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -36,4 +36,64 @@ $(function(){
       }
     })
   }
-})
+
+
+  //2.点击添加分类按钮, 显示添加模态框
+  $('#addBtn').click(function(){
+    //显示添加模态框
+    $('#addModal').modal('show');
+  });
+
+  //3.添加表单校验功能
+  $('#form').bootstrapValidator({
+
+    //配置校验图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok', //校验成功
+      invalid: 'glyphicon glyphicon-remove', //校验失败
+      validating: 'glyphicon glyphicon-refresh' //校验中
+    },
+
+    //字段列表
+    fields: {
+      categoryName: {
+        //校验规则, 要求非空
+        validators: {
+          notEmpty: {
+            message: "请输入一级分类名称"
+          }
+        }
+      }
+    }
+  });
+
+  //4.添加表单校验成功事件,阻止默认的表单提交, 通过ajax提交
+  $('#form').on('success.form.bv',function(e){
+    //阻止默认提交
+    e.preventDefault();
+    
+    //通过ajax提交
+    $.ajax({
+      type: "post",
+      url: "/category/addTopCategory",
+      data: $('#form').serialize(),
+      dataType: "json",
+      success:function(info){
+        console.log(info)
+
+        if( info.success ){
+          //添加成功
+          //关闭模态框
+          $('#addModal').modal('hide');
+          //重新渲染第一页
+          currentPage = 1;
+          render();
+
+          //重置表单内容和状态
+          $('#form').data('bootstrapValidator').resetForm(true);
+        }
+      }
+    })
+  })
+  
+});
